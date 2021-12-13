e8889cda0c929c5b42b0915fe19927fea3ddd092
wang0077 <228913299@qq.com>
2021-12-13 21:56:03 +0800

    add init

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -1,13 +1,16 @@
 package com.example.jenkins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
+        log.info("Success");
     }
 
 }
